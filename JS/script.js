f86f2f82638f986ea36531ffea34a458b587dbc4
diegosald <diegosalday@gmail.com>
2024-05-17 16:19:32 -0300

    act 17/5

---
+++ JS/script.js
@@ -0,0 +1,32 @@
+const cajaOferta = document.getElementById("oferta")
+const imgBomba = document.getElementById("bomba")
+const imgExplosion = document.getElementById("explosion")
+
+cajaOferta.addEventListener("mouseover",function (){
+   
+    //alert("alalalalala");
+    imgBomba.classList.add("d-none")
+    imgExplosion.classList.remove("d-none");
+})
+cajaOferta.addEventListener("mouseout",function (){
+   
+   // alert("alalalalala");
+    imgExplosion.classList.add("d-none")
+    imgBomba.classList.remove("d-none");
+})
+
+
+const botonesCard = document.getElementsByClassName("card-body")
+
+for (let boton of botonesCard){
+        boton.getElementsByClassName("btn")[0].innerText = "MAS INFO"
+        boton.getElementsByClassName("btn")[0].addEventListener("click",function(){
+        var cajatexto = document.getElementById("validationTextarea");
+        var epadre = boton.getElementsByClassName("btn")[0].parentElement;
+        var nombreMoto = epadre.getElementsByClassName("card-title")[0].textContent
+        cajatexto.innerText = "QUIERO MAS INFO SOBRE " + nombreMoto
+    
+    }
+    )
+}
+
